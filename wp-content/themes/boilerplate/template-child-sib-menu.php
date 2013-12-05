b525e0997a75847283885aed45a31ea470e5d5be
MP <consiler@gmail.com>
2013-12-04 20:13:50 -0500

    made internal page sidenav highlight the current page in blue

--- wp-content/themes/boilerplate/template-child-sib-menu.php
+++ wp-content/themes/boilerplate/template-child-sib-menu.php
@@ -1,6 +1,9 @@
 <?php
 $myID = get_the_ID();
 
+//this makes the link to current page blue
+$current_page_class = "current-page";
+
 //this menu is based on parent-child relationships of the pages themselves
 //it's a backup for when we can't get information from the top nav menu
 function dlg_default_sidebar_menu() {
@@ -26,9 +29,9 @@ function dlg_default_sidebar_menu() {
       $children_count++;
       $child_href = get_page_link($child->ID);
       $child_title = $child->post_title;
-      ?>
-        <li><a href="<?php echo $child_href; ?>"><?php echo $child_title; ?></a></li>
-      <?php
+        echo '<li><a ';
+        if($child->ID == $myID) echo 'class="'.$current_page_class.'" ';
+        echo 'href="'.$child_href.'">'.$child_title.'</a></li>';
     }
   }
 }
@@ -50,11 +53,15 @@ if($nav_items = wp_get_nav_menu_items('Top Navigation')) {
   //otherwise print the children/siblings of this page
   else {
     //if this is a top level page, print its children
-    //else print its siblings
+    //otherwise print its siblings
     if(!$parent) $parent = $myNavID;
     foreach ($nav_items as $sb_menu_item) {
-      if ($sb_menu_item->menu_item_parent == $parent)
-        echo '<li><a href="'.$sb_menu_item->url.'">'.$sb_menu_item->title.'</a></li>';
+      if ($sb_menu_item->menu_item_parent == $parent) {
+        echo '<li><a ';
+        if ($sb_menu_item->ID == $myNavID)
+          echo 'class="'.$current_page_class.'" ';
+        echo 'href="'.$sb_menu_item->url.'">'.$sb_menu_item->title.'</a></li>';
+      }
     }
   }
 }
